372d25dc6375ba2bb001dfeb06421cb0fa8fa45c
Monica <monica.dixon8@gmail.com>
2020-05-02 12:15:43 -0500

    Updates

---
+++ precache-manifest.4e574d5b9a2c14c4b232040d8220c1a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0414f407e1464b9620a90b5f8f3ec682",
+    "url": "/yoga/index.html"
+  },
+  {
+    "revision": "1862f9468fcc8eda6411",
+    "url": "/yoga/static/css/main.9e0a16c4.chunk.css"
+  },
+  {
+    "revision": "ed88b4e985cbd4974366",
+    "url": "/yoga/static/js/2.eb43a2b3.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/yoga/static/js/2.eb43a2b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1862f9468fcc8eda6411",
+    "url": "/yoga/static/js/main.a5d4139e.chunk.js"
+  },
+  {
+    "revision": "011e758a07e5d73a3fc4",
+    "url": "/yoga/static/js/runtime-main.53c4eb90.js"
+  },
+  {
+    "revision": "3425a7cea6a3ee5b91ff08ffb7d11fa2",
+    "url": "/yoga/static/media/1.3425a7ce.gif"
+  }
+]);
